e9719eaa622ed8ce3e7c407973d0168268e2b135
Sijtech <joesiewerdt@gmail.com>
2018-05-18 13:13:41 +0200

    PowerShell exercises

---
+++ PowerShell/Aufgabe3.ps1
@@ -0,0 +1,11 @@
+﻿#Aufgabe3
+#a
+Get-Date -UFormat %V
+Get-Date -Year 1992 -Month 08 -Day 15 -UFormat %V
+#b
+(Get-Date).AddHours(-24)
+#c
+Get-EventLog -LogName System -After (Get-Date).AddHours(-24) -EntryType Error, Warning | Out-GridView
+#d
+Get-Service -name S* | Where-Object {$_.status -eq "running"}
+Get-Service S* | Where status -eq "running"
---
+++ PowerShell/SMAS_STFW_PowerShell-Skript-Grundlagen.ps1
@@ -0,0 +1,15 @@
+﻿#SMAS_STFW_PowerShell-Skript-Grundlagen.pdf
+
+#Aufgabe 1, Seite 57
+Get-EventLog -LogName System -EntryTyp Error,Warning -Newest 15 -Message *Dienst*
+Get-EventLog -LogName System -EntryTyp Error,Warning -Message *Dienst* -Index 7429 | Format-Table -Wrap
+
+#Aufgabe 2, Seite 67
+Get-Date -UFormat %d.%m.%Y #Datum anzeigen udn formatieren
+Get-Date -DisplayHint Date #Datum anzeigen
+Get-Date -UFormat %R #Zeit anzeigen
+Get-Date -DisplayHint Time #Zeit anzeigen
+Get-Date -UFormat %Y%m%d #Datum anzeigen udn formatieren
+Get-Date -UFormat %b #gekürzten Monat anzeigen (z.B: Apr)
+Get-Date -Year 1992 -Month 08 -Day 15 -UFormat %A #Tag eines bestimmten Datums anzeigen
+
